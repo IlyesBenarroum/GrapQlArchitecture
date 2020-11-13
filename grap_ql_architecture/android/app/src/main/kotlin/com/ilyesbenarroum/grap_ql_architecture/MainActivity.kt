730728159f384a73bb3016bf67ec99d37daec8a7
Name <iliesbenarroum@gmail.com>
2020-11-13 09:42:43 +0100

    first commit

---
+++ grap_ql_architecture/android/app/src/main/kotlin/com/ilyesbenarroum/grap_ql_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ilyesbenarroum.grap_ql_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ grap_ql_architecture/lib/app/modules/home/bindings/home_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'package:grap_ql_architecture/app/modules/home/controllers/home_controller.dart';
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+  }
+}
